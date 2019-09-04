749a952288877086da329e65cd2bc4d4dab01666
Dmitry Sazonov <sazon@nxt.ru>
2019-09-04 14:21:21 +0300

    order + qr

--- src/Lime/Request.php
+++ src/Lime/Request.php
@@ -11,8 +11,10 @@ class Request
     const INSTALLATION_ID = '79';
     const CASHDESK_ID = '210';
     const FILENAME = 'tokens';
+    const REFRESH_TIME = '3600';
+    const PROCESSING_ID = '161';
 
-    function query($url, $postFields, $headers = null)
+    function query($url, $postFields, $headers = null, $method = 'POST')
     {
         $ch        = curl_init();
         curl_setopt($ch, CURLOPT_URL, self::API_URL . $url);
@@ -21,10 +23,13 @@ class Request
         }
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        if($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+        }
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //curl_setopt($ch, CURLOPT_VERBOSE, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         $response = curl_exec($ch);
         curl_close($ch);
@@ -59,9 +64,7 @@ class Request
         if($tokens = $this->getTokens()) {
             return $tokens;
         } else {
-            $tokens = $this->auth();
-            $this->memorizeTokens($tokens);
-            return $tokens;
+            return $this->newTokens();
         }
     }
 
@@ -133,24 +136,40 @@ class Request
     private function getTokens()
     {
         if(file_exists(self::FILENAME)) {
+
             $tokens = file_get_contents(self::FILENAME);
+
             if($tokens) {
+
                 $tokens = explode(';', $tokens);
-                if(is_array($tokens) && count($tokens) == 2) {
-                    return [
-                        'access_token' => $tokens[0],
-                        'refresh_token' => $tokens[1]
-                    ];
+
+                if(is_array($tokens) && count($tokens) == 3) {
+                    if(time() - $tokens[2] > self::REFRESH_TIME) {
+                        return $this->newTokens();
+                    } else {
+                        return [
+                            'access_token' => $tokens[0],
+                            'refresh_token' => $tokens[1],
+                            'time' => $tokens[2]
+                        ];
+                    }
                 }
             }
         }
         return [];
     }
 
+    private function newTokens()
+    {
+        $tokens = $this->auth();
+        $this->memorizeTokens($tokens);
+        return $tokens;
+    }
+
     private function memorizeTokens($tokens)
     {
         if(isset($tokens['access_token']) && isset($tokens['refresh_token'])) {
-            $tokens = implode(';', [$tokens['access_token'], $tokens['refresh_token']]);
+            $tokens = implode(';', [$tokens['access_token'], $tokens['refresh_token'], time()]);
             file_put_contents(self::FILENAME, $tokens);
         }
     }
@@ -165,22 +184,178 @@ class Request
                 "Authorization: Bearer " . $accessToken,
                 "Content-Type: application/json"
             ];
-            return $this->query("/api/CashdeskServer/GetAllGoodTypesAndCategoriesForInstallation?cashdeskId=" . self::CASHDESK_ID . "&installationId=" . self::INSTALLATION_ID, [], $headers);
+            return $this->query("/api/CashdeskServer/GetAllGoodTypesAndCategoriesForInstallation?cashdeskId=" . self::CASHDESK_ID . "&installationId=" . self::INSTALLATION_ID, [], $headers, 'GET');
         }
     }
 
-    public function order(array $items)
+    public function generateCard()
     {
-        $currentShift = $this->currentShift();
-        print_r($currentShift);
-        if(count($currentShift) && $cashdeskId = $currentShift['shift']['id']) {
-            $limeItems = $this->items();
-            echo "<pre>";
-            print_r($limeItems);
-            die();
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/GenerateCard?cashdeskId=" . self::CASHDESK_ID . "&installationId=" . self::INSTALLATION_ID, [], $headers);
         }
-        var_dump($currentShift);
-        die();
+    }
+
+    public function putCheck($postData)
+    {
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/PutCheck", $postData, $headers);
+        }
+    }
+
+    public function generateQr($cardId)
+    {
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/GenerateQr?cardId=" . $cardId, [], $headers, 'GET');
+        }
+    }
+
+    public function serverTime()
+    {
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/GetServerCurrentTime", [], $headers, 'GET');
+        }
+    }
+
+    public function getNextCheckId()
+    {
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/GetNextCheckId", [], $headers, 'GET');
+        }
+    }
+
+    public function generateRight($buildQuery)
+    {
+        $tokens = $this->init();
+        if(isset($tokens['access_token'])) {
+
+            $accessToken = $tokens['access_token'];
+            $headers   = [
+                "Authorization: Bearer " . $accessToken,
+                "Content-Type: application/json"
+            ];
+            return $this->query("/api/CashdeskServer/GetOrCreateRight?". $buildQuery, [], $headers, 'GET');
+        }
+    }
+
+    public function order($items)
+    {
+        $time = $this->serverTime();
+        $checkId = $this->getNextCheckId();
+
+        $order = $qrs = [];
+        $order['order'] = [];
+        for ($j = 0; $j < sizeof($items); $j++) {
+            $card = $this->generateCard();
+            if($card && $cardId = $card['card']['id']) {
+                $qrCode = $this->generateQr($cardId);
+                $vars           = [];
+                $vars['cardId'] = $cardId;
+                $vars['cashdeskId'] = self::CASHDESK_ID;
+                $vars['goodTypeId'] = $items[$j]["limeid"];
+                $vars['price'] = $items[$j]["price"];
+                $vars['time']       = $time;
+                $right = $this->generateRight(http_build_query($vars));
+                $vars['rightId'] = $right['id'];
+                $vars['qrCode'] = $qrCode;
+                $order['order'][] = $vars;
+                $qrs[] = $qrCode;
+            }
+        }
+        $limeOrder = [];
+        $limeOrder['check'] = [
+            'installationTime' => substr($time, 0, 19),
+            'id' => $checkId,
+            'installationId' => self::INSTALLATION_ID,
+            'cashdeskId' => self::CASHDESK_ID,
+            'userId' => self::API_USER_ID,
+            'time' => $time,
+            'type' => 0,
+            'administrative' => false
+        ];
+
+        for ($j = 0; $j < sizeof($order['order']); $j++) {
+            $entry = [
+                'entry' => $checkId,
+                'id' => 0,
+                'amount' => 1,
+                'rightId' => $order['order'][$j]['rightId'],
+                'cardId' => $order['order'][$j]['cardId'],
+                'goodTypeId' => $order['order'][$j]['goodTypeId'],
+                'price' => $this->gettextbal($order['order'][$j]["price"]),
+                'basePrice' => $this->gettextbal($order['order'][$j]["price"]),
+                'returnCheckEntryId' => null,
+                'printedInQr' => true,
+                'count' => 1
+            ];
+
+            $limeOrder["entries"][$j]['entry'] = $entry;
+
+            $payments = [
+                'checkEntryId' => 0,
+                'id' => 0,
+                'processingId' => self::PROCESSING_ID,
+                'amount' => $this->gettextbal($order['order'][$j]["price"]),
+                'transactionReference' => 'Genbank',
+                'checkEntryDiscounts' => ""
+            ];
+            $limeOrder["entries"][$j]["payments"][0] = $payments;
+            $limeOrder["entries"][$j]["checkEntryDiscounts"] = [];
+        }
+        $limeOrder["cardChanges"] = [];
+        $postLimeOrder = json_encode($limeOrder);
+        $this->putCheck($postLimeOrder);
+        return $qrs;
+    }
+
+    function gettextbal($b)
+    {
+        if($b==0)
+        {
+            $r="0.00";
+        }
+        else
+        {
+            $r="".($b/100)."";
+            $t=explode(".",$r);
+            if(!isset($t[1])){$t[1]="00";}else{if(strlen($t[1])==1){$t[1]=$t[1]."0";}}
+            $r=implode(".",$t);
+        }
+        return $r;
     }
 }
 
--- tests/test.php
+++ tests/test.php
@@ -1,8 +1,23 @@
 <?php 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/QR/qrlib.php';
 use Lime\Request;
+//use Lime\Qr;
 $request = new Request();
+$items = [
+    [
+        "id" => 0,
+        "cname" => "kids",
+        "name" => "Детский билет",
+        "price" => 10600,
+        "limeid" => 1065
+    ]
+];
+$qrs = $request->order($items);
+//$qr = new Qr();
+foreach($qrs as $qr) {
+    QRcode::png($qr, __DIR__ . "/tickets/1.png", QR_ECLEVEL_L, 10);
+}
 
-$items = [];
-print_r($request->order($items));
-die();
+//$generated = $qr->setQrCodes($qrs)->generate();
+//print_r($generated);
